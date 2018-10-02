45f32bfde90f7287960696116b3b3b8fb19d7129
Eric Vo <vo.eric@gmail.com>
2018-10-01 22:12:20 -0400

    add routes to see all user profiles and a specific user profile by id or handle

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -36,6 +36,67 @@ router.get('/', passport.authenticate('jwt', {
     }).catch(err => res.status(404).json(err));
 });
 
+// @route     GET api/profile/all
+// @desc      Get all user profiles
+// @access    Public
+router.get('/all', (req, res) => {
+  const errors = {};
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = 'There are no profiles';
+        return res.status(404).json(errors);
+      }
+
+      res.json(profiles);
+    }).catch(err => {
+      res.status(404).json({
+        profile: 'There are no profiles'
+      })
+    })
+});
+
+
+// @route     GET api/profile/handle/:handle
+// @desc      Get specific user's profile via handle
+// @access    Public
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+  Profile.findOne({
+      handle: req.params.handle
+    })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    }).catch(err => res.status(404).json(err));
+});
+
+// @route     GET api/profile/user/:user_id
+// @desc      Get specific user's profile via id
+// @access    Public
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({
+      user: req.params.user_id
+    })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no associated profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    }).catch(err => res.status(404).json({
+      profile: 'There is no associated profile for this user'
+    }));
+});
+
 // @route     POST api/profile
 // @desc      Create / Edit user profile
 // @access    Private
