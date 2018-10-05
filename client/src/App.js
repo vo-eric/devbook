655388cbe41ebab246c9e7b72eb0c9d2137d5968
Eric Vo <vo.eric@gmail.com>
2018-10-04 23:59:16 -0400

    add frontend auth with validation

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Provider } from "react-redux";
+import jwt_decode from "jwt-decode";
 
 import store from "./store";
 import Navbar from "./components/layout/Navbar";
@@ -8,8 +9,22 @@ import Footer from "./components/layout/Footer";
 import Landing from "./components/layout/Landing";
 import Register from "./components/auth/Register";
 import Login from "./components/auth/Login";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
 
 import "./App.css";
+
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  const decoded = jwt_decode(localStorage.jwtToken);
+  const currentTime = Date.now() / 1000;
+  store.dispatch(setCurrentUser(decoded));
+
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = "/login";
+  }
+}
 
 class App extends Component {
   render() {
