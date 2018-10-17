4954a4ce671443812bb25da24d00fab5e734f0b9
Eric Vo <vo.eric@gmail.com>
2018-10-17 14:09:55 -0400

    hid keys

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://Eric:password1@ds113703.mlab.com:13703/devconnect",
-  secretOrKey: "tupelo",
-  clientId: "c2008e8e094d77fcf872",
-  clientSecretId: "666b466e74b05fc4bb0cba7e3c3ae56ea5b318ba"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
